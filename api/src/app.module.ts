12a82c0ff77249f17526854ded2b20459f0cd845
rambutan <rambutan@educonlines.com>
2021-06-05 14:22:13 +0300

    add modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PageModule } from './page/page.module';
+import { OrderModule } from './order/order.module';
+import { LayoutModule } from './layout/layout.module';
 
 @Module({
-	imports: [],
+	imports: [AuthModule, PageModule, OrderModule, LayoutModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
