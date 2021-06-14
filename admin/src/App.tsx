9b33d140778814442691ba0b29585ab789dd818f
rambutan <rambutan@educonlines.com>
2021-06-14 12:06:03 +0300

    initialize admin && added fix to front

---
+++ admin/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = (): JSX.Element => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				dfgdfgdfg
+			</header>
+		</div>
+	);
+};
+
+export default App;
