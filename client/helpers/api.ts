978e116b6892439460f4994fe1063b39ec200d21
rambutan <rambutan@educonlines.com>
2021-11-07 13:00:19 +0200

    fix

--- client/helpers/api.ts
+++ client/helpers/api.ts
@@ -1,4 +1,10 @@
 export const API = {
 	admin: process.env.NODE_ENV === 'development' ? 'http://admin.arter.local' : 'http://constructor.chost.com.ua',
 	host: process.env.NODE_ENV === 'development' ? 'http://admin.arter.local' : 'http://constructor.chost.com.ua',
+	pages: {
+		home: 'api/page/slag/home',
+		404: 'api/page/slag/404',
+		login: 'api/auth/login',
+		registration: 'api/auth/register'
+	},
 };
